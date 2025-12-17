90cfca3d631bdff170bb12e6854c6ea3f58ae768
Christofer Laurin <christofer.laurin@gmail.com>
2025-12-17 16:06:27 +0100

    fix: move icons to correct folders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import icons from '../../static/icons.svg?raw';
+	import icons from '$lib/assets/icons.svg?raw';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@html icons}
 {@render children()}
